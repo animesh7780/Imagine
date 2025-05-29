cf39a8d4f139a13a0d92375fa36c586a1aa37a2e
Animesh Kumar <143063970+animesh7780@users.noreply.github.com>
2025-05-29 23:16:46 +0530

    New

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Home from './pages/Home'
+import Result from './pages/REsult'
+import BuyCredit from './pages/BuyCredit'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b frm-teal-50 to-orange-50'>
+
+      <Navbar/>
+
+      <Routes>
+
+      <Route path='/' element = {<Home/>}/>
+      <Route path='/result' element = {<Result/>}/>
+      <Route path='/credits' element = {<BuyCredit/>}/>
+       
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/pages/BuyCredit.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BuyCredit = () => {
+  return (
+    <div>
+      Credits
+    </div>
+  )
+}
+
+export default BuyCredit
