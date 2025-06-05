2ff53d1d874b62e704e3c05d9c9a2e0ec4285d3f
Animesh Kumar <143063970+animesh7780@users.noreply.github.com>
2025-06-06 01:14:57 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Login from './components/Login'
 import { AppContext } from './context/AppContext'
+import {}
 
 const App = () => {
 
--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,11 +2,38 @@ import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
 import { motion } from 'motion/react'
+import axios from 'axios'
 
 const Login = () => {
 
     const [state, setState] = useState('Login')
-    const {setShowLogin} = useContext(AppContext)
+    const {setShowLogin, backendUrl, setToken, setUser} = useContext(AppContext)
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onSubmitHandler = async (e)=>{
+        e.preventDefault();
+
+        try {
+            
+            if(state === 'Login'){
+                const {data} = await axios.post(backendUrl + '/api/user/login', {email, password})
+                
+                if(data.success){
+                    setToken(data.token)
+                    setUser(data.user)
+                    localStorage.setItem('token', data.token)
+                    setShowLogin(false) 
+                }else{
+                    
+                }
+            }
+
+        } catch (error) {
+            
+        }
+    }
 
     useEffect(()=>{
         document.body.style.overflow = 'hidden';
@@ -21,7 +48,8 @@ const Login = () => {
     <div className='fixed top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'
 
     >
-        <motion.form className='relative bg-white p-10 rounded-xl text-slate-500'
+        <motion.form onSubmit={onSubmitHandler}
+            className='relative bg-white p-10 rounded-xl text-slate-500'
             initial = {{opacity:0.2, y:100}}
             transition={{duration:1}}
             whileInView={{opacity:1, y:0}}
@@ -32,17 +60,17 @@ const Login = () => {
 
             {state != 'Login' && <div className='border px-6 py-2 flex items-center fap-2 rounded-full mt-4'>
                 <img src={assets.twitter_icon} alt="" />
-                <input type="text" className='outline-none text-sm' placeholder='name' required/>
+                <input onChange={e => setName(e.target.value)} value={name} type="text" className='outline-none text-sm' placeholder='name' required/>
             </div>}
 
             <div className='border px-6 py-2 flex items-center fap-2 rounded-full mt-5'>
                 <img src={assets.email_icon} alt="" />
-                <input type="email" className='outline-none text-sm' placeholder='email' required/>
+                <input onChange={e => setEmail(e.target.value)} value={email} type="email" className='outline-none text-sm' placeholder='email' required/>
             </div> 
 
             <div className='border px-6 py-2 flex items-center fap-2 rounded-full mt-4'>
                 <img src={assets.lock_icon} alt="" />
-                <input type="password" className='outline-none text-sm' placeholder='password' required/>
+                <input onChange={e => setPassword(e.target.value)} value={password} type="password" className='outline-none text-sm' placeholder='password' required/>
             </div> 
 
             <p className='test-sm text-blue-600 my-4 cursor-pointer'>Forgot Password?</p>
